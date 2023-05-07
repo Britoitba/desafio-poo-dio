87675e40af5082bd3e0abebc41936999888f8280
Francisco Nascimento <felipe.ryo@hotmail.com>
2023-05-06 21:58:15 -0300

    Adicionando Bootcamp e Dev

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,17 @@ import br.com.dio.desafio.dominio.Mentoria;
 import java.time.LocalDate;
 
 public class Main {
+
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
 
-        Curso curso2 = new Curso("Curso JS", "descrição curso JS", 8);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("descrição curso JS");
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
